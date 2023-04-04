24cc5cbd2bcedf0c31faa46d8685c7220d8fca8b
swayam thummar <127782240+swayamthummar@users.noreply.github.com>
2023-04-04 17:01:04 +0530

    Add files via upload

---
+++ Project-Assortment-3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+main()
+{
+	int a[]={1,2,3,4,5},i,o,l,n=4;
+	int length=sizeof(a)/sizeof(a[0]);
+
+	printf("The value fo Array->\n");
+	
+	for(i=0;i<length;i++)
+	{
+		printf("%d  ",a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		l=a[length-1];
+		
+	   for(o=length-1;o>0;o--)
+	   {
+		  a[o]=a[o-1];
+	   }
+		a[0]=l;
+	}
+	printf("\n");
+	printf("Right Rotation of Array->\n");
+	
+	for(i=0;i<length;i++)
+	{
+		printf("%d  ",a[i]);
+	}
+}
